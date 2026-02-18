d9a062e5204b699f8001b7f3860b50ed23bb166e
itziarZG <itziar.zameza.dev@gmail.com>
2026-02-18 09:53:18 +0100

    fix: fix header buttons height

--- components/LinkButton.tsx
+++ components/LinkButton.tsx
@@ -16,8 +16,8 @@ export default function LinkButton({
       href={href}
       className={
         variant === 'big'
-          ? 'bg-primary hover:bg-primary/90 text-[#111813] px-12 py-5 rounded-2xl text-xl font-black transition-all shadow-xl shadow-primary/20 w-full flex flex-col md:flex-row items-center justify-center '
-          : 'bg-primary hover:bg-primary/90 text-[#111813] px-8 py-4 rounded-xl text-lg font-bold transition-all shadow-lg shadow-primary/20 flex items-center justify-center gap-2'
+          ? 'bg-primary hover:bg-primary/90 text-[#111813] px-8 py-3 rounded-2xl text-xl font-black transition-all shadow-xl shadow-primary/20 w-full flex flex-col md:flex-row items-center justify-center '
+          : 'bg-primary hover:bg-primary/90 text-[#111813] px-2 py-1 rounded-xl text-lg font-bold transition-all shadow-lg shadow-primary/20 flex items-center justify-center gap-2'
       }
     >
       {children}
