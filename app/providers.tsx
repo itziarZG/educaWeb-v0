dd3ef329e3b383f33f7ad892a90148094cc176a6
Itziar ZG <70289074+itziarZG@users.noreply.github.com>
2026-03-16 13:46:30 +0100

    fix: fix registers and logins (#45)

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,6 +3,7 @@
 import { ReactNode } from 'react';
 import { ThemeProvider } from '@/components/theme-provider';
 import { AuthProvider } from '@/context/auth-context';
+import { Toaster } from 'sonner';
 
 import { User } from '@supabase/supabase-js';
 
@@ -14,15 +15,16 @@ export function Providers({
   initialUser?: User | null;
 }) {
   return (
-    <AuthProvider initialUser={initialUser} key={initialUser?.id}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <AuthProvider initialUser={initialUser} key={initialUser?.id}>
         {children}
-      </ThemeProvider>
-    </AuthProvider>
+      </AuthProvider>
+      <Toaster position="top-center" richColors />
+    </ThemeProvider>
   );
 }
