5454c6f6f334dd609864bb47ed1bfe210c0a6797
itziarZG <itziar.zameza.dev@gmail.com>
2026-03-16 14:30:10 +0100

    feat: add xxs size and fix login buttons in very small screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -99,6 +99,9 @@ const config: Config = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      screens: {
+        xxs: '375px',
+      },
     },
   },
   plugins: [tailwindcssAnimate],
